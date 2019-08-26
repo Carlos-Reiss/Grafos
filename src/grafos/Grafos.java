dbfc2cdc8fc0e03bf83bfef233273e752968330a
Carlos-Reiss <carlos_reis10.06@hotmail.com>
2019-08-26 17:01:10 -0300

    version 0.0.2

--- src/grafos/Grafos.java
+++ src/grafos/Grafos.java
@@ -3,13 +3,25 @@ package grafos;
 import ListaAdj.Lista_de_adja;
 import MatrizAdj.MatrizAdj;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Grafos {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         ArrayList vet = new ArrayList();
         Arq arquivo = new Arq(); 
         vet = arquivo.Arq();       
+        System.out.println("Arquivo Carregado...");
+        
+        menu();
+        int op = input.nextInt();
+        if(op == 1){
+            
+        }
+        if(op == 2){
+            
+        }
         /*
             for(int i = 0; i < vet.size();i++){
                System.out.println(vet.get(i));
@@ -22,4 +34,11 @@ public class Grafos {
             }
         */                       
     }    
+
+    private static void menu() {
+        System.out.println("========================");
+        System.out.println("1 - Lista Adjacencias  ");
+        System.out.println("2 - Matriz Adjacencias ");
+        System.out.print("Digite sua Opcão:");
+    }
 }
